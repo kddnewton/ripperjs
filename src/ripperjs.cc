b920dbb838dc038b5717dc55d105fe3b5905d7e4
Kevin Deisz <kevin.deisz@gmail.com>
2018-05-21 10:15:30 -0400

    Build node objects, not just plain results

--- src/ripperjs.cc
+++ src/ripperjs.cc
@@ -2,62 +2,125 @@
 #include <ruby.h>
 
 namespace ripperjs {
+  using v8::Isolate;
   using v8::Local;
 
-  Local<v8::Array> arrayToV8(v8::Isolate *isolate, VALUE sexps) {
-    const long sexpsLen = RARRAY_LEN(sexps);
-    Local<v8::Array> result = v8::Array::New(isolate, sexpsLen);
+  using v8::Array;
+  using v8::Integer;
+  using v8::Object;
+  using v8::String;
+  using v8::Value;
 
-    VALUE sexp;
-    const char *cstr;
+  Local<Value> objectToTree(Isolate *isolate, VALUE object);
 
-    for (long idx = 0; idx < sexpsLen; idx++) {
-      sexp = rb_ary_entry(sexps, idx);
+  Local<Object> arrayToLiteral(Isolate *isolate, VALUE array) {
+    Local<Object> literal = Object::New(isolate);
+    VALUE location = rb_ary_entry(array, 2);
 
-      switch (TYPE(sexp)) {
-        case T_SYMBOL:
-          cstr = rb_id2name(SYM2ID(sexp));
-          result->Set(idx, v8::String::NewFromUtf8(isolate, cstr));
-          break;
-        case T_FALSE:
-          result->Set(idx, v8::False(isolate));
-          break;
-        case T_NIL:
-          result->Set(idx, v8::Null(isolate));
-          break;
-        case T_FIXNUM:
-          result->Set(idx, v8::Integer::New(isolate, FIX2LONG(sexp)));
-          break;
-        case T_STRING:
-          cstr = StringValueCStr(sexp);
-          result->Set(idx, v8::String::NewFromUtf8(isolate, cstr));
-          break;
-        case T_ARRAY:
-          result->Set(idx, arrayToV8(isolate, sexp));
-          break;
-      }
-    }
+    literal->Set(
+      String::NewFromUtf8(isolate, "type"),
+      String::NewFromUtf8(isolate, rb_id2name(SYM2ID(rb_ary_entry(array, 0))))
+    );
 
-    return result;
+    literal->Set(
+      String::NewFromUtf8(isolate, "body"),
+      objectToTree(isolate, rb_ary_entry(array, 1))
+    );
+
+    literal->Set(
+      String::NewFromUtf8(isolate, "lineno"),
+      Integer::New(isolate, FIX2LONG(rb_ary_entry(location, 0)))
+    );
+
+    literal->Set(
+      String::NewFromUtf8(isolate, "colno"),
+      Integer::New(isolate, FIX2LONG(rb_ary_entry(location, 1)))
+    );
+
+    return literal;
   }
 
-  Local<v8::Array> codeToV8(v8::Isolate *isolate, const char *code) {
+  Local<Object> arrayToNode(Isolate *isolate, VALUE array) {
+    Local<Object> node = Object::New(isolate);
+
+    node->Set(
+      String::NewFromUtf8(isolate, "type"),
+      String::NewFromUtf8(isolate, rb_id2name(SYM2ID(rb_ary_entry(array, 0))))
+    );
+
+    long nodeBodySize = RARRAY_LEN(array) - 1;
+    Local<Array> nodeBody = Array::New(isolate, nodeBodySize);
+
+    long idx;
+    for (idx = 1; idx <= nodeBodySize; idx++) {
+      nodeBody->Set(idx - 1, objectToTree(isolate, rb_ary_entry(array, idx)));
+    }
+
+    node->Set(String::NewFromUtf8(isolate, "body"), nodeBody);
+
+    return node;
+  }
+
+  Local<Array> arrayToArray(Isolate *isolate, VALUE array) {
+    long size = RARRAY_LEN(array);
+    Local<Array> node = Array::New(isolate, size);
+
+    long idx;
+    for (idx = 0; idx < size; idx++) {
+      node->Set(idx, objectToTree(isolate, rb_ary_entry(array, idx)));
+    }
+
+    return node;
+  }
+
+  Local<Value> objectToTree(Isolate *isolate, VALUE object) {
+    switch (TYPE(object)) {
+      case T_SYMBOL:
+        return String::NewFromUtf8(isolate, rb_id2name(SYM2ID(object)));
+      case T_FALSE:
+        return v8::False(isolate);
+      case T_NIL:
+        return v8::Null(isolate);
+      case T_FIXNUM:
+        return Integer::New(isolate, FIX2LONG(object));
+      case T_STRING:
+        return String::NewFromUtf8(isolate, StringValueCStr(object));
+      case T_ARRAY:
+        if (RARRAY_LEN(object) == 0) {
+          return Array::New(isolate, 0);
+        }
+
+        VALUE name = rb_ary_entry(object, 0);
+
+        switch (TYPE(name)) {
+          case T_SYMBOL:
+            if (rb_id2name(SYM2ID(name))[0] == '@') {
+              return arrayToLiteral(isolate, object);
+            }
+            return arrayToNode(isolate, object);
+          case T_ARRAY:
+            return arrayToArray(isolate, object);
+        }
+    }
+  }
+
+  Local<Value> codeToV8(Isolate *isolate, const char *code) {
     ruby_init();
     ruby_init_loadpath();
     rb_require("ripper");
 
     VALUE rb_cRipper = rb_const_get(rb_cObject, rb_intern("Ripper"));
-    VALUE sexps = rb_funcall(
+    VALUE array = rb_funcall(
       rb_cRipper, rb_intern("sexp"), 1, rb_str_new2(code)
     );
-    return arrayToV8(isolate, sexps);
+    return objectToTree(isolate, array);
   }
 
-  void Sexp(const v8::FunctionCallbackInfo<v8::Value>& args) {
-    v8::Isolate* isolate = args.GetIsolate();
+  void Sexp(const v8::FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
 
     if (args.Length() != 1) {
-      Local<v8::String> error = v8::String::NewFromUtf8(
+      Local<String> error = String::NewFromUtf8(
         isolate, "Wrong number of arguments"
       );
       isolate->ThrowException(v8::Exception::TypeError(error));
@@ -65,18 +128,18 @@ namespace ripperjs {
     }
 
     if (!args[0]->IsString()) {
-      Local<v8::String> error = v8::String::NewFromUtf8(
+      Local<String> error = String::NewFromUtf8(
         isolate, "Code must be a string"
       );
       isolate->ThrowException(v8::Exception::TypeError(error));
       return;
     }
 
-    v8::String::Utf8Value code(isolate, args[0]->ToString());
+    String::Utf8Value code(isolate, args[0]->ToString());
     args.GetReturnValue().Set(codeToV8(isolate, *code));
   }
 
-  void init(Local<v8::Object> exports) {
+  void init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "sexp", Sexp);
   }
 
