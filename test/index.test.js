39b6c99434da3f0ee97dff18a481514ce0c5c40b
Kevin Deisz <kevin.deisz@gmail.com>
2018-05-21 10:24:28 -0400

    Fix up tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const { sexp } = require("../src");
 
 test("converts to s-expressions", () => {
-  const [type] = sexp("a = 1; b = 2; a + b");
-  expect(type).toEqual(":program");
+  const { type } = sexp("a = 1; b = 2; a + b");
+  expect(type).toEqual("program");
 });
